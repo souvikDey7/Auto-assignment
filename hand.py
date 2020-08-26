9054fb149a1c3c40ce06bfbc87df5a7473af2d23
Souvik Dey <63603276+souvikDey7@users.noreply.github.com>
2020-08-27 02:44:45 +0530

    Add files via upload

--- hand.py
+++ hand.py
@@ -4,10 +4,16 @@ Created on Thu Aug 27 00:21:36 2020
 #d:\anaconda\lib\site-packages
 @author: deyso
 """
-
 from PIL import Image
 import pytesseract
-#pytesseract.pytesseract.tesseract_cmd="D:\Anaconda\Lib\site-packages\pytesseract"
-pic=Image.open("pic.jpg")
-text=pytesseract.image_to_string(pic)
-print(text)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\deyso\AppData\Local\Tesseract-OCR\tesseract.exe'
+import opencv as cv
+pic2=Image.open("pic2.jpg")
+pic1=Image.open("pic1.jpg")
+print("start")
+e=cv.hconcat([pic2, pic1])
+print(e)
+#text=pytesseract.image_to_string(pic,lang="eng")
+#for i in text:
+#    print(i)
+#print("done")
